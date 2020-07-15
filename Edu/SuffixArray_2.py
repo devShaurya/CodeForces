2a206867f1e0f554e8d7c418feb2561e7d0c43d7
Agarawal, Shaurya <shaurya.agarawal@accenture.com>
2020-07-15 21:13:23 +0530

    Radix sort for sorting pairs

--- Edu/SuffixArray_2.py
+++ Edu/SuffixArray_2.py
@@ -1,3 +1,44 @@
+### use Radix sort to sort the pairs, by using counting sort twice. First on right class then on left class
+### Algorthm is of complexity O(nlogn)
+
+
+def radix_sort(pairs):
+
+	## sort according to a.right
+	right_counts=[0]*len(pairs)
+
+	for i in range(len(pairs)):
+		right_counts[pairs[i][1]]+=1
+
+	right_indexes=[0]*len(pairs)
+	for i in range(1,len(pairs)):
+		right_indexes[i]=right_counts[i-1]+right_indexes[i-1]
+
+	sort_right_pairs=[0]*len(pairs)
+	for i in range(len(pairs)):
+		sort_right_pairs[right_indexes[pairs[i][1]]]=pairs[i]
+		right_indexes[pairs[i][1]]+=1
+
+	# copy array
+	pairs=sort_right_pairs.copy()
+	
+	# sort acc to left elements
+	left_counts=[0]*len(pairs)
+
+	for i in range(len(pairs)):
+		left_counts[pairs[i][0]]+=1
+
+	left_indexes=[0]*len(pairs)
+	for i in range(1,len(pairs)):
+		left_indexes[i]=left_counts[i-1]+left_indexes[i-1]
+
+	sort_left_pairs=[0]*len(pairs)
+	for i in range(len(pairs)):
+		sort_left_pairs[left_indexes[pairs[i][0]]]=pairs[i]
+		left_indexes[pairs[i][0]]+=1
+	
+	return sort_left_pairs
+
 def solve(string):
 	string=string+'$'
 	pairs=[[string[i],i] for i in range(len(string))]
@@ -15,10 +56,12 @@ def solve(string):
 	## k->k+1
 	k=0
 	while((1<<k)<len(string)):
+		### make pairs
 		for i in range(len(string)):
 			pairs[i]=[classes[i],classes[(i+(1<<k))%len(string)],i]
-
-		pairs.sort()
+		### sort pairs using radix sort
+		pairs=radix_sort(pairs)
+		## make equivalence classes 
 		classes=[0]*len(string)
 		for i in range(1,len(pairs)):
 			if(pairs[i][0]==pairs[i-1][0] and pairs[i][1]==pairs[i-1][1]):
@@ -29,8 +72,8 @@ def solve(string):
 	return pairs
 
 def main():
-	# s=input()
-	s="ababba"
+	s=input()
+	# s="ababba"
 	suffix_array=solve(s)
 	for i in range(len(suffix_array)):
 		print(suffix_array[i][2],end=' ')
